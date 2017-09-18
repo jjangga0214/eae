93252a3c46c83e2cd4fdd2cbd87ba421aa9d3886
jjangga <jjangga@kookmin.ac.kr>
2017-09-18 10:35:17 +0900

    edit ip_adjustment
    set basic logic of beginning

--- ip_adjustment.py
+++ ip_adjustment.py
@@ -23,4 +23,14 @@ files = ["./server/cali_server.py",
 
 expected_cwd = "Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi"
 
-
+if __name__ == "__main__":
+    cwd_name = parse_cwd_name()
+    if parse_cwd_name() != expected_cwd:
+        print("Error : 디렉토리 오류 \nError Message : " + expected_cwd + " 폴더에서 실행해야 합니다.")
+    else:
+        print("작업이 시작되었습니다")
+        print()
+        ip = get_ipaddress()
+        for file in files:
+            with open(file, "r+") as f:
+                print(f.read())
